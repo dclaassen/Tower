ba9d9c94c99fa9fe9f82abb76e8f0b4681b99a9c
DC <dclaassen4444@gmail.com>
2016-02-23 16:22:58 -0600

    Start point for  app integration. SuperUI selections added for overwatch app

--- Android/src/org/droidplanner/android/activities/OverwatchActivity.java
+++ Android/src/org/droidplanner/android/activities/OverwatchActivity.java
@@ -26,5 +26,54 @@ public class OverwatchActivity extends AppCompatActivity{
         return true;
     }
 
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
 
+        //On whichever click from the menu do one of the following
+        switch ((id)) {
+            case R.id.menu_bluetooth:
+                try {
+                    bluetoothSetupSwitchScreensClick();
+                    break;
+                } catch (Exception e) {
+                    showMessage("bluetoothSetupSwitchScreensClick() E ERROR");
+                }
+
+                break;
+            case R.id.menu_btcontroller:
+                try {
+                    bluetoothRemoteControlSwitchScreensClick();
+                    break;
+                } catch (Exception e) {
+                    showMessage("bluetoothSerialCommunicationsSwi...() E ERROR");
+                }
+            default:
+                ;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    //Method which prints a message in the form of a toast (Black Message Box) to the screen
+    private void showMessage(String theMsg) {
+        Toast msg = Toast.makeText(getBaseContext(),
+                theMsg, (Toast.LENGTH_SHORT));
+        msg.show();
+    }
+
+    //Method which creates the link to and start the Bluetooth Setup screen/activity
+    public void bluetoothSetupSwitchScreensClick() throws Exception {
+        Intent bluetoothSetupIntent = new Intent(OverwatchActivity.this, BluetoothSetup.class);
+        startActivity(bluetoothSetupIntent);
+    }
+
+    //Method which creates the link to and start the Bluetooth Remote Control screen/activity
+    public void bluetoothRemoteControlSwitchScreensClick() throws Exception{
+        Intent bluetoothRemoteControlIntent = new Intent(OverwatchActivity.this, BluetoothRemoteControl.class);
+        startActivity(bluetoothRemoteControlIntent);
+    }
 }
